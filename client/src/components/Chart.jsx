0066d5063bee29f6e93f2ed3c38c79a968840705
Kevin Price <Kevin.Price@hidigital.solutions>
2020-04-14 12:10:31 -0700

    Refactors Bar chart into Line chart and simplifies logic for readability

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,11 +1,9 @@
 import React, { PureComponent } from 'react';
-import {
-  ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-} from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Chart extends PureComponent {
-
   constructor(props) {
     super(props);
 
@@ -13,49 +11,60 @@ class Chart extends PureComponent {
       temperatures: [],
     };
   }
-  render() {
 
+  render() {
     const { temperatures } = this.state;
     const { observations } = this.props;
 
     observations.forEach(observation => {
-      const date = new Date(observation.date).toISOString().slice(0, 10);
-      let temp; 
+      const datetime = new Date(observation.date).toISOString().slice(0, 10);
+      let temp;
       if (
-            !observation.physical.feverSeverity ||
-            observation.physical.feverSeverity === '' ||
-            !parseFloat(observation.physical.feverSeverity, 10) > 0
-          ) {
-            temp = 0;
-          } else {
-            temp = parseFloat(observation.physical.feverSeverity, 10);
-          }
+        !observation.physical.feverSeverity ||
+        observation.physical.feverSeverity === '' ||
+        !parseFloat(observation.physical.feverSeverity, 10) > 0
+      ) {
+        temp = 0;
+      } else {
+        temp = parseFloat(observation.physical.feverSeverity, 10);
+      }
 
-  
-      var tempRecord = { date: date, temperature: temp };
+      const tempRecord = { date: datetime, temperature: temp };
       temperatures.push(tempRecord);
     });
-    
+
     return (
-      <ResponsiveContainer width='100%' aspect={4.0/1.5}>
-      <LineChart
-        data={temperatures}
-        margin={{
-          top: 10, right: 50, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis type="number" domain={[90, 110]}/>
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="temperature" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Tooltip />
-      </LineChart>
+      <ResponsiveContainer width="100%" aspect={4.0 / 1.5}>
+        <LineChart
+          data={temperatures}
+          margin={{
+            top: 10,
+            right: 50,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis type="number" domain={[90, 110]} />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="temperature" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Tooltip />
+        </LineChart>
       </ResponsiveContainer>
     );
   }
 }
+
+Chart.propTypes = {
+  observations: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      temperature: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 const mapStateToProps = state => {
   return {
